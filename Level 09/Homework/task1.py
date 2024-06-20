7ff88ccb2bebd753e2c2548f8cf391fbafea9d1a
nikobaramidze17@gmail.com <nikobaramidze17@gmail.com>
2024-06-20 18:14:53 +0400

    commit

---
+++ Level 09/Homework/task1.py
@@ -0,0 +1,28 @@
+#1) დაწერეთ პითონის პროგრამა, რომელიც მოუწოდებს მომხმარებელს შეიყვანოს ორი რიცხვი და შემდეგ დაბეჭდოს მათი ჯამი
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+sum = num1 + num2
+
+#2)დაწერეთ პითონის პროგრამა, რომელიც ითვლის მართკუთხედის ფართობს. პროგრამამ უნდა სთხოვოს მომხმარებელს მართკუთხედის სიგანე და სიმაღლე.
+def calculate_area(width, height):
+ width * height
+
+# ჩასმა რიცხვების
+width = float(input("Enter the width of the rectangle: "))
+height = float(input("Enter the height of the rectangle: "))
+
+#გამოთვლა
+area = calculate_area(width, height)
+
+#3) დაწერეთ პითონის პროგრამა, რომელიც მოუწოდებს მომხმარებელს შეიყვანოს რიცხვი და შემდეგ დაბეჭდოს ამ რიცხვის ორმაგი.
+num1 = input("Enter the first number: ")
+num2 = input("Enter the second number: ")
+
+num1*num2
+num3 = input("Input the anwser: ")
+
+
+
+
+
+
