c1caeaf29af8484ba2bc0fff38769441f11d3d3b
nikobaramidze17@gmail.com <nikobaramidze17@gmail.com>
2024-06-27 19:40:45 +0400

    commit

---
+++ Level 13/Classwork/task1.py
@@ -0,0 +1,41 @@
+# String
+# Integer
+# Float
+# Boolean - True/False - ჭეშმარიტი/მცდარი
+
+
+
+
+
+# Logical Operatos - 'and' Operator === 'or' operator
+
+#     inp1     inp2
+print(True and True)  # True
+print(True and False) # False
+print(False and True) # False
+print(False and False) # False
+
+
+
+
+
+
+# OR operator
+
+#     inp1     inp2
+print(True and True)  # True
+print(True and False) # True
+print(False and True) # True
+print(False and False) # False
+
+
+
+
+
+
+# The program takes the score as an input
+score = int(input('enter ur score:'))
+
+
+# Add the comparison operation inside these parentheses
+print(True and False)
