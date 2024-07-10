81df902f56ecec2090b8a136b41f4ba2ac3344ab
nikobaramidze17@gmail.com <nikobaramidze17@gmail.com>
2024-07-10 22:07:22 +0400

    commit

--- Level 15/Homework/task1.py
+++ Level 15/Homework/task1.py
@@ -1,22 +1,4 @@
-import random
-
-def guess_number(user_number):
-    if 1 <= user_number <= 1000:
-        guessed_number = random.randint(1, 1000)
-        if guessed_number == user_number:
-            print('სწორია!')
-        else
-            print('არასწორია!')
-        else
-        print('გთხოვთ შეიყვანოთ რიცხვი 1 - დან 1000 - მდე.')
-
-
-try:
-    user_input = int(input("შეიყვანეთ რიცხვი 1 - დან 1000 - მდე: "))
-    guess_number(user_input)
-except ValueError:
-    print("გთხოვთ შეიყვანოთ ვალიდური რიცხვი.")
-
+num int = input ('input ur number: ')
 
 
 
